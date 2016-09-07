787dcd307ab7f99f8e0165176ac5485dea80e9dd
johfr035 <johfr035@student.liu.se>
2016-09-07 22:01:11 +0200

    New try to get it working on openshift..

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
@@ -14,6 +18,6 @@ app.use('/fonts', express.static('fonts'));
 app.use('/img', express.static('img'));
 
 
-http.listen(8080, function(){
-    console.log('listening on *:8080');
+http.listen(server_port, server_ip_address, function(){
+    console.log('listening on ' + server_ip_address + ': ' + server_port);
 });
